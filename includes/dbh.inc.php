2ffaccf43d5ebba6ec76505782849673b3852fe6
Seonghoon (Martin) Park <martin941079@gmail.com>
2024-02-16 19:50:10 +0900

    fixed apache2 issue- the apache2 server now doesn't show raw html; page loads properly

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,13 +7,11 @@ $dBName = "butcherStore";
 
 if (mysqli_connect_errno()){
     echo "Failed to Connect to MySQL: ", mysqli_connect_error();
+    if(!$conn) {
+        // if connection not created
+        // kill the connection
+        die("Connection failed:".mysqli_connect_error());
+    }
     exit();
 }
-
-
-// if(!$conn) {
-//     // if connection not created
-//     // kill the connection
-//     die("Connection failed:".mysqli_connect_error());
-// }
 ?>
